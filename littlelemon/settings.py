9da78a538c5f6361048120ec1d0163871c7fa379
Yardley Estiverne <52601743+fyardlest1@users.noreply.github.com>
2023-06-12 07:52:30 -0400

    Setting up the models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,10 +74,25 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# MySQL connection
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "littlelemon",
+        "PORT": '3306',
+        "HOST": "localhost",
+        "USER": "admindjango",
+        "PASSWORD": "employee@123!",
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }      
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    no_of_guests = models.SmallIntegerField(default=10)
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    menu_description = models.TextField(max_length=1000, default="")
+
+    def __str__(self):
+        return self.title
