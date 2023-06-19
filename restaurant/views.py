9e8253ca87298fbc28fcd1ef5a8c8b8b768d4385
Yardley Estiverne <52601743+fyardlest1@users.noreply.github.com>
2023-06-19 08:42:40 -0400

    Setting up the User

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-from .serializers import MenuSerializer, BookingSerializer
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 
 from .models import Booking, Menu
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class MenuItemView(ListCreateAPIView):
